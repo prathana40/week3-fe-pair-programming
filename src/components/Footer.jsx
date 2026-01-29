f41982f14fcd1e7a76bbe265688a16be26069bfe
Prathana <prathanay40@gmail.com>
2026-01-29 15:06:42 +0530

    iteration7

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,55 +1,52 @@
+ import PageLinks from './PageLinks'; // make sure the path is correct
+
 function Footer() {
   return (
-  <>
-  {/* Footer  section starts */}
-  <footer className="section footer">
-    	  <ul className="footer-links" id="nav-links">
-            <li><a href="#home" className="footer-link">home</a></li>
-            <li><a href="#about" className="footer-link">about</a></li>
-            <li><a href="#services" className="footer-link">services</a></li>
-            <li><a href="#tours" className="footer-link">tours</a></li>
-      </ul>
-    <ul className="footer-icons">
-      <li>
-        <a
-          href="https://www.facebook.com"
-          target="_blank"
-          rel="noreferrer"
-          className="footer-icon"
-        >
-          <i className="fab fa-facebook" />
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://www.twitter.com"
-          target="_blank"
-          rel="noreferrer"
-          className="footer-icon"
-        >
-          <i className="fab fa-twitter" />
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://www.squarespace.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="footer-icon"
-        >
-          <i className="fab fa-squarespace" />
-        </a>
-      </li>
-    </ul>
-    <p className="copyright">
-      copyright © travel tours company <span id="date" />. all rights reserved
-    </p>
-  </footer>
-  {/* Footer  section starts */}
-</>
+    <>
+      {/* Footer section starts */}
+      <footer className="section footer">
+        {/* Replace static links with reusable component */}
+        <PageLinks parentClass="footer-links" itemClass="footer-link" />
 
+        <ul className="footer-icons">
+          <li>
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noreferrer"
+              className="footer-icon"
+            >
+              <i className="fab fa-facebook" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noreferrer"
+              className="footer-icon"
+            >
+              <i className="fab fa-twitter" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.squarespace.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="footer-icon"
+            >
+              <i className="fab fa-squarespace" />
+            </a>
+          </li>
+        </ul>
+        <p className="copyright">
+          copyright © travel tours company <span id="date" />. all rights reserved
+        </p>
+      </footer>
+      {/* Footer section ends */}
+    </>
   );
 }
 
 export default Footer;
- 
--- src/components/PageLinks.jsx
+++ src/components/PageLinks.jsx
@@ -1,13 +1,14 @@
-import { pageLinks } from '../data'
+ import { pageLinks } from '../data'
 import PageLink from './PageLink'
 
 const PageLinks = ({ parentClass, itemClass }) => {
   return (
     <ul className={parentClass} id='nav-links'>
       {pageLinks.map((link) => {
-        return<PageLinks parentClass='footer-links' itemClass='footer-link' />
+        return <PageLink key={link.id} link={link} itemClass={itemClass} />
       })}
     </ul>
   )
 }
+
 export default PageLinks
