04326ef32239b579b87793d41e59fb22b6ca8886
AFirose <afrin.techpm@gmail.com>
2026-01-29 11:16:18 +0200

    Iteration 6:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,82 +1,23 @@
 import logo from '../assets/images/logo.svg'
+import PageLinks from './PageLinks'
+import SocialLinks from './SocialLinks'
 
 const Navbar = () => {
   return (
-    
-       <>
-  {/* Navbar section starts */}
-  <div>
-    <nav className="navbar">
-      <div className="nav-center">
-        <div className="nav-header">
-           <img src={logo} className='nav-logo' alt='backroads' />
-          <button type="button" className="nav-toggle" id="nav-toggle">
-            <i className="fas fa-bars" />
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} className='nav-logo' alt='backroads' />
+          <button type='button' className='nav-toggle' id='nav-toggle'>
+            <i className='fas fa-bars'></i>
           </button>
         </div>
-        <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link">
-              home
-            </a>
-          </li>
-          <li>
-            <a href="#about" className="nav-link">
-              about
-            </a>
-          </li>
-          <li>
-            <a href="#services" className="nav-link">
-              services
-            </a>
-          </li>
-          <li>
-            <a href="#tours" className="nav-link">
-              tours
-            </a>
-          </li>
-        </ul>
-        <ul className="nav-icons">
-          <li>
-            <a
-              href="https://www.facebook.com"
-              target="_blank"
-              rel="noreferrer"
-              className="nav-icon"
-            >
-              <i className="fab fa-facebook" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.x.com"
-              target="_blank"
-              rel="noreferrer"
-              className="nav-icon"
-            >
-              <i className="fab fa-x-twitter" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.squarespace.com/"
-              target="_blank"
-              rel="noreferrer"
-              className="nav-icon"
-            >
-              <i className="fab fa-squarespace" />
-            </a>
-          </li>
-        </ul>
+
+        <PageLinks parentClass='nav-links' itemClass='nav-link' />
+        <SocialLinks parentClass='nav-icons' itemClass='nav-icon' />
       </div>
     </nav>
-    {/* Navbar section ends */}
-  </div>
-</>
+  )
+}
 
-    
-  );
-};
-
-export default Navbar;
- 
+export default Navbar
--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -1,6 +1,16 @@
-function SocialLink() {
-  return <div>SocialLink</div>;
+const SocialLink = ({ link, itemClass }) => {
+  return (
+    <li>
+      <a
+        href={link.href}
+        target='_blank'
+        rel='noreferrer'
+        className={itemClass}
+      >
+        <i className={link.icon}></i>
+      </a>
+    </li>
+  )
 }
 
-export default SocialLink;
- 
+export default SocialLink
