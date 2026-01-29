815a6bda3100e9c0997ae0a152da7ab67bad1186
AFirose <afrin.techpm@gmail.com>
2026-01-29 11:53:47 +0200

    Iteration 8: CSS Partial

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,18 @@
+import React from 'react';
+import styles from './Hero.module.css'; // Import the CSS module
+
 function Hero() {
   return (
     <>
   {/* Hero section starts */}
-  <section className="hero" id="home">
-    <div className="hero-banner">
+  <section className={styles.hero} id="home">
+    <div className={styles['hero-banner']}>
       <h1>backroads app</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae explicabo
         debitis est autem dicta.
       </p>
-      <a href="#tours" className="btn hero-btn">
+      <a href="#tours" className={styles['hero-btn']}>
         explore tours
       </a>
     </div>
